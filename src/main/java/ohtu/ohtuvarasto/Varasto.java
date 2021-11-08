07749b8a180b770e7e753920f6556768a82f3e60
Laura Ruuska <laura.ruuska@helsinki.fi>
2021-11-08 20:38:40 +0200

    Created paaohjelma

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -1,4 +1,4 @@
-package ohtu.ohtuvarasto;
+package paaohjelma;
 
 public class Varasto {
 
--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -1,4 +1,4 @@
-package ohtu.ohtuvarasto;
+package paaohjelma;
 
 import org.junit.*;
 import static org.junit.Assert.*;
